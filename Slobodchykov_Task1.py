18d583915e7fae5778f60b2ac9dd4303e18fea32
Jalkhet <118116354+Max1mj@users.noreply.github.com>
2023-03-08 11:16:13 +0300

    Add files via upload

---
+++ Slobodchykov_Task1.py
@@ -0,0 +1,5 @@
+print("'Introduction to programming':Task 1")
+print(" Maksym Slobodchykov, IKM-221a")
+print(44*"Vladislav Ovsianikov, ","Vladislav Ovsianikov.")
+print("Show rusult:")
+print(7.8 -((1+(2.1/3))/(78+21.3)))
